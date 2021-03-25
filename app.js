c2a546cbdd809914d57afc0a706a0ed24da2d970
bbrad3 <68521389+bbrad3@users.noreply.github.com>
2021-03-25 10:13:08 -0600

    Created getData function in app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log('hello from app.js')
+
+
+const url = 'http://myapi-profstream.herokuapp.com/api/c4a880/books'
+
+async function getData(url) {
+    let res = await fetch(url)
+    let data = await res.json()
+    console.log(data)
+}
+getData(url)
